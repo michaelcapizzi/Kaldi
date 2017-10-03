a5ed1ff18c44498ff647fcb0b2be8a6f8b4a0cc1
Michael Capizzi <mcapizzi@email.arizona.edu>
2017-10-02 18:25:50 -0700

    still debugging openfst

--- docker/build_container.sh
+++ docker/build_container.sh
@@ -5,13 +5,18 @@
 
 # OPTIONAL ARGUMENTS
 # -g = set GPU_SUPPORT to true
+# -m = set amount of memory docker can use during build
 
 gpu=false
+mem=2g
 
-while getopts "g" opt; do
+while getopts "gm:" opt; do
     case ${opt} in
         g)
             gpu=true
+            ;;
+        m)
+            mem=${OPTARG}
             ;;
         \?)
             echo "Wrong flags"
@@ -23,9 +28,13 @@ done
 if [[ ${gpu} == true ]]; then
     docker build \
         -f GPU/Dockerfile \
+        -m ${mem} \
+        --build_arg MEM=${mem} \
         -t docker-kaldi-instructional ../
 else
     docker build \
         -f CPU/Dockerfile \
+        -m ${mem} \
+        --build_arg MEM=${mem} \
         -t docker-kaldi-instructional ../
 fi

298d04670a477eceb97367a901c4468f5699f5d6
Michael Capizzi <mcapizzi@email.arizona.edu>
2017-10-03 09:03:01 -0700

    redoing tensorflow_cc install

--- egs/INSTRUCTIONAL/path.sh
+++ egs/INSTRUCTIONAL/path.sh
@@ -21,18 +21,18 @@ PYTHON='python2.7'
 ### Below are the paths used by the optional parts of the recipe
 
 # We only need the Festival stuff below for the optional text normalization(for LM-training) step
-FEST_ROOT=tools/festival
-NSW_PATH=${FEST_ROOT}/festival/bin:${FEST_ROOT}/nsw/bin
-export PATH=$PATH:$NSW_PATH
+#FEST_ROOT=tools/festival
+#NSW_PATH=${FEST_ROOT}/festival/bin:${FEST_ROOT}/nsw/bin
+#export PATH=$PATH:$NSW_PATH
 
 # SRILM is needed for LM model building
-SRILM_ROOT=$KALDI_ROOT/tools/srilm
-SRILM_PATH=$SRILM_ROOT/bin:$SRILM_ROOT/bin/i686-m64
-export PATH=$PATH:$SRILM_PATH
+#SRILM_ROOT=$KALDI_ROOT/tools/srilm
+#SRILM_PATH=$SRILM_ROOT/bin:$SRILM_ROOT/bin/i686-m64
+#export PATH=$PATH:$SRILM_PATH
 
 # Sequitur G2P executable
-sequitur=$KALDI_ROOT/tools/sequitur/g2p.py
-sequitur_path="$(dirname $sequitur)/lib/$PYTHON/site-packages"
+#sequitur=$KALDI_ROOT/tools/sequitur/g2p.py
+#sequitur_path="$(dirname $sequitur)/lib/$PYTHON/site-packages"
 
 # Directory under which the LM training corpus should be extracted
-LM_CORPUS_ROOT=./lm-corpus
+#LM_CORPUS_ROOT=./lm-corpus

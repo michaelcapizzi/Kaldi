eee6133882b3a55dc04c55720dd5fbcc699cc1c9
Michael Capizzi <mcapizzi@email.arizona.edu>
2017-10-05 15:08:49 -0700

    getting further on the Dockerfile

--- tools/extras/install_tensorflow_cc.sh
+++ tools/extras/install_tensorflow_cc.sh
@@ -2,39 +2,49 @@
 
 set -e
 
-PATH=$PATH:$PWD/bazel/output
-#export HOME=$PWD/tensorflow_build/
-#mkdir -p $HOME
+#PATH=$PATH:$PWD/bazel/output
+##export HOME=$PWD/tensorflow_build/
+##mkdir -p $HOME
+#
+#java=`which java`
+#if [ "$JAVA_HOME" != "" ]; then
+#  java=$JAVA_HOME/bin/java
+#fi
+#
+#java_version=`$java -version 2>&1 | head -n 1 | awk '{print $3}' | sed s=\"==g`
+#good_version=`echo 1.8 $java_version | awk '{if($1<$2)print 1; else print 0}'`
+#if [ $good_version -eq 0 ]; then
+#  echo You have jdk version = $java_version, which is older than 1.8
+#  echo You need to download a later than 1.8 JDK version at
+#  echo http://www.oracle.com/technetwork/pt/java/javase/downloads/jdk8-downloads-2133151.html
+#  echo and set your JAVA_HOME to point to where it is installed
+#  exit 1
+#else
+#  echo You have jdk version = $java_version, which is newer than 1.8. We will continue the installation
+#fi
+#
+#
+#[ ! -f bazel.zip ] && wget --no-check-certificate https://github.com/bazelbuild/bazel/releases/download/0.5.4/bazel-0.5.4-dist.zip -O bazel.zip
+#mkdir -p bazel
+#cd bazel
+#unzip ../bazel.zip
+#./compile.sh
+#cd ../
 
-java=`which java`
-if [ "$JAVA_HOME" != "" ]; then
-  java=$JAVA_HOME/bin/java
-fi
+ $1 <bool> install with GPU support?
 
-java_version=`$java -version 2>&1 | head -n 1 | awk '{print $3}' | sed s=\"==g`
-good_version=`echo 1.8 $java_version | awk '{if($1<$2)print 1; else print 0}'`
-if [ $good_version -eq 0 ]; then
-  echo You have jdk version = $java_version, which is older than 1.8
-  echo You need to download a later than 1.8 JDK version at
-  echo http://www.oracle.com/technetwork/pt/java/javase/downloads/jdk8-downloads-2133151.html
-  echo and set your JAVA_HOME to point to where it is installed
-  exit 1
+if [[ ${1} == "true" ]]; then
+    tf_type=gpu
 else
-  echo You have jdk version = $java_version, which is newer than 1.8. We will continue the installation
+    tf_type=cpu
 fi
 
-
-[ ! -f bazel.zip ] && wget --no-check-certificate https://github.com/bazelbuild/bazel/releases/download/0.5.4/bazel-0.5.4-dist.zip -O bazel.zip
-mkdir -p bazel
-cd bazel
-unzip ../bazel.zip
-./compile.sh
-cd ../
-
-# now bazel is built
+# assumes bazel is already built
 git clone https://github.com/tensorflow/tensorflow
 cd tensorflow
-#echo "Y Y N N Y N Y Y N" | ./your_script
+if [[ ${tf_type} == "true" ]]; then
+    export TF_NEED_CUDA=1
+fi
 ./configure
 
 tensorflow/contrib/makefile/download_dependencies.sh
@@ -49,13 +59,7 @@ echo \"make\" under tensorflow/ and tfbin/ to generate the binaries
 bazel build --config=opt //tensorflow:libtensorflow.so
 bazel build --config=opt //tensorflow:libtensorflow_cc.so
 
-# $1 <bool> install with GPU support?
-#
-#if [[ ${1} == "true" ]]; then
-#    tf_type=gpu
-#else
-#    tf_type=cpu
-#fi
+
 #
 ## check for expected variable
 #if [ -z ${KALDI_PATH} ]; then

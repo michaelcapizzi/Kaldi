9e247d4094e63c9136abbf68cd0f74ec55e7524a
Michael Capizzi <mcapizzi@email.arizona.edu>
2017-10-11 10:32:16 -0700

    builds compiled kaldi (no tf)

--- docker/build_container.sh
+++ docker/build_container.sh
@@ -6,22 +6,18 @@
 # OPTIONAL ARGUMENTS
 # -g = set GPU_SUPPORT to true
 # -m = set amount of memory docker can use during build (in G)
-# -t = set number of threads to use during compilation steps
 
 gpu=false
 mem=4g
 threads=2
 
-while getopts "gm:t:" opt; do
+while getopts "gm:" opt; do
     case ${opt} in
         g)
             gpu=true
             ;;
         m)
             mem=${OPTARG}
-            ;;
-        t)
-            threads=${OPTARG}
             ;;
         \?)
             echo "Wrong flags"
@@ -34,12 +30,10 @@ if [[ ${gpu} == true ]]; then
     docker build \
         -f GPU/Dockerfile \
         -m ${mem} \
-        --build-arg THREADS=${threads} \
         -t docker-kaldi-instructional ../
 else
     docker build \
         -f CPU/Dockerfile \
         -m ${mem} \
-        --build-arg THREADS=${threads} \
         -t docker-kaldi-instructional ../
 fi

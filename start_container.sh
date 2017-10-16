5cc0b99b246505ebb9678abfa3d7fa5bdbfc9565
Michael Capizzi <mcapizzi@email.arizona.edu>
2017-10-16 11:57:44 -0700

    got to compile but error during mkgrph on mac

--- start_container.sh
+++ start_container.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# starts existing container
+# must currently be run from kaldi root
+
+# cannot link all the way at kaldi root directory
 docker run -it -p 8888:8888 \
-    -v `pwd`:/home/kaldi/ \
+    -v `pwd`/egs:/home/kaldi/egs \
     docker-kaldi-instructional

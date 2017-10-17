5f5c573bd00d1938eb705d190aba2db4f110da91
Michael Capizzi <mcapizzi@email.arizona.edu>
2017-10-17 12:34:19 -0700

    updating run script

--- egs/mini_librispeech/s5/run.sh
+++ egs/mini_librispeech/s5/run.sh
@@ -8,7 +8,7 @@ data=${KALDI_PATH}/egs/mini_librispeech/raw_data
 # -c <str> = which command to use (run.pl or queue.pl)
 
 stage=0
-num_proc=2
+num_proc=1
 train_cmd=run.pl
 
 while getopts "s:n:c:" opt; do
@@ -36,7 +36,7 @@ lm_url=www.openslr.org/resources/11
 
 . $KALDI_PATH/egs/mini_librispeech/s5/path.sh
 
-. $KALDI_PATH/egs/mini_librispeech/utils/parse_options.sh
+. $KALDI_PATH/egs/mini_librispeech/s5/utils/parse_options.sh
 
 set -euo pipefail
 
@@ -83,69 +83,66 @@ fi
 if [ $stage -le 3 ]; then
   steps/train_mono.sh --boost-silence 1.25 --nj $num_proc --cmd "$train_cmd" --num_iters 2 --totgauss 500 \
     data/train_500short data/lang_nosp exp/mono
-  (
-    utils/mkgraph.sh data/lang_nosp_test_tgsmall \
-      exp/mono exp/mono/graph_nosp_tgsmall
-    for test in dev_clean_2; do
-      steps/decode.sh --nj $num_proc --cmd "$decode_cmd" exp/mono/graph_nosp_tgsmall \
-        data/$test exp/mono/decode_nosp_tgsmall_$test
-    done
-  )&
-
+#  (
+#    utils/mkgraph.sh data/lang_nosp_test_tgsmall \
+#      exp/mono exp/mono/graph_nosp_tgsmall
+#    for test in dev_clean_2; do
+#      steps/decode.sh --nj $num_proc --cmd "$decode_cmd" exp/mono/graph_nosp_tgsmall \
+#        data/$test exp/mono/decode_nosp_tgsmall_$test
+#    done
+#  )&
+  utils/mkgraph.sh data/lang_nosp_test_tgsmall exp/mono exp/mono/graph_nosp_tgsmall
+  steps/decode.sh --nj $num_proc --cmd "$decode_cmd" exp/mono/graph_nosp_tgsmall \
+    data/dev_clean_2 exp/mono/decode_nosp_tgsmall_dev_clean_2
   steps/align_si.sh --boost-silence 1.25 --nj $num_proc --cmd "$train_cmd" \
     data/train_500short data/lang_nosp exp/mono exp/mono_ali_train_500short
 fi
 
 # train a first delta + delta-delta triphone system on all utterances
-# TODO do everything on train_500short
-#if [ $stage -le 4 ]; then
-#  steps/train_deltas.sh --boost-silence 1.25 --cmd "$train_cmd" \
-#    2000 10000 data/train_clean_5 data/lang_nosp exp/mono_ali_train_clean_5 exp/tri1
-#
-  # decode using the tri1 model
+if [ $stage -le 4 ]; then
+  steps/train_deltas.sh --boost-silence 1.25 --cmd "$train_cmd" --num_iters 2 \
+    2000 10000 data/train_500short data/lang_nosp exp/mono_ali_train_500short exp/tri1
+
+   # decode using the tri1 model
 #  (
 #    utils/mkgraph.sh data/lang_nosp_test_tgsmall \
 #      exp/tri1 exp/tri1/graph_nosp_tgsmall
 #    for test in dev_clean_2; do
-#      steps/decode.sh --nj 5 --cmd "$decode_cmd" exp/tri1/graph_nosp_tgsmall \
+#      steps/decode.sh --nj $num_proc --cmd "$decode_cmd" exp/tri1/graph_nosp_tgsmall \
 #      data/$test exp/tri1/decode_nosp_tgsmall_$test
-#      steps/lmrescore.sh --cmd "$decode_cmd" data/lang_nosp_test_{tgsmall,tgmed} \
-#        data/$test exp/tri1/decode_nosp_{tgsmall,tgmed}_$test
-#      steps/lmrescore_const_arpa.sh \
-#        --cmd "$decode_cmd" data/lang_nosp_test_{tgsmall,tglarge} \
-#        data/$test exp/tri1/decode_nosp_{tgsmall,tglarge}_$test
 #    done
 #  )&
-#
-#  steps/align_si.sh --nj 5 --cmd "$train_cmd" \
-#    data/train_clean_5 data/lang_nosp exp/tri1 exp/tri1_ali_train_clean_5
-#fi
+
+  utils/mkgraph.sh data/lang_nosp_test_tgsmall exp/tri1 exp/tri1/graph_nosp_tgsmall
+  steps/decode.sh --nj $num_proc --cmd "$decode_cmd" exp/tri1/graph_nosp_tgsmall \
+    data/dev_clean_2 exp/tri1/decode_nosp_tgsmall_dev_clean_2
+  steps/align_si.sh --nj $num_proc --cmd "$train_cmd" \
+    data/train_500short data/lang_nosp exp/tri1 exp/tri1_ali_train_500short
+fi
 
 # train an LDA+MLLT system.
-#if [ $stage -le 5 ]; then
-#  steps/train_lda_mllt.sh --cmd "$train_cmd" \
-#    --splice-opts "--left-context=3 --right-context=3" 2500 15000 \
-#    data/train_clean_5 data/lang_nosp exp/tri1_ali_train_clean_5 exp/tri2b
-#
+if [ $stage -le 5 ]; then
+  steps/train_lda_mllt.sh --cmd "$train_cmd" --num_iters 2 \
+    --splice-opts "--left-context=3 --right-context=3" 2500 15000 \
+    data/train_500short data/lang_nosp exp/tri1_ali_train_500short exp/tri2b
+
 #   decode using the LDA+MLLT model
 #  (
 #    utils/mkgraph.sh data/lang_nosp_test_tgsmall \
 #      exp/tri2b exp/tri2b/graph_nosp_tgsmall
 #    for test in dev_clean_2; do
-#      steps/decode.sh --nj 10 --cmd "$decode_cmd" exp/tri2b/graph_nosp_tgsmall \
+#      steps/decode.sh --nj $num_proc --cmd "$decode_cmd" exp/tri2b/graph_nosp_tgsmall \
 #        data/$test exp/tri2b/decode_nosp_tgsmall_$test
-#      steps/lmrescore.sh --cmd "$decode_cmd" data/lang_nosp_test_{tgsmall,tgmed} \
-#        data/$test exp/tri2b/decode_nosp_{tgsmall,tgmed}_$test
-#      steps/lmrescore_const_arpa.sh \
-#        --cmd "$decode_cmd" data/lang_nosp_test_{tgsmall,tglarge} \
-#        data/$test exp/tri2b/decode_nosp_{tgsmall,tglarge}_$test
 #    done
 #  )&
-#
-#  # Align utts using the tri2b model
-#  steps/align_si.sh  --nj 5 --cmd "$train_cmd" --use-graphs true \
-#    data/train_clean_5 data/lang_nosp exp/tri2b exp/tri2b_ali_train_clean_5
-#fi
+
+  utils/mkgraph.sh data/lang_nosp_test_tgsmall exp/tri2b exp/tri2b/graph_nosp_tgsmall
+  steps/decode.sh --nj $num_proc --cmd "$decode_cmd" exp/tri2b/graph_nosp_tgsmall \
+    data/dev_clean_2 exp/tri2b/decode_nosp_tgsmall_dev_clean_2
+  # Align utts using the tri2b model
+  steps/align_si.sh  --nj $num_proc --cmd "$train_cmd" --use-graphs true \
+    data/train_clean_5 data/lang_nosp exp/tri2b exp/tri2b_ali_train_clean_5
+fi
 #
 ## Train tri3b, which is LDA+MLLT+SAT
 #if [ $stage -le 6 ]; then

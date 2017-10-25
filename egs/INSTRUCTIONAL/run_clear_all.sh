7f05b4b58f0e644f2916a158b2ab7a6a04cb889f
root <mcapizzi@email.arizona.edu>
2017-10-25 16:39:36 -0700

    beginning week 3

---
+++ egs/INSTRUCTIONAL/run_clear_all.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# This script removes all files generated in `nextiva_recipes` after any step of the process.
+# Ensures a clean state
+
+rm -rf ${KALDI_INSTRUCTIONAL_PATH}/data/ \
+    ${KALDI_INSTRUCTIONAL_PATH}/mfcc/ \
+    ${KALDI_INSTRUCTIONAL_PATH}/exp/
+

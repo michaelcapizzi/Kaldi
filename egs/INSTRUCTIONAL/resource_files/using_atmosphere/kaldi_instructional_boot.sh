ff6bada8113f091e7e881891fdadac93bdd02010
Michael Capizzi <mcapizzi@email.arizona.edu>
2017-12-27 04:17:24 -0700

    updated boot script

--- egs/INSTRUCTIONAL/resource_files/using_atmosphere/kaldi_instructional_boot.sh
+++ egs/INSTRUCTIONAL/resource_files/using_atmosphere/kaldi_instructional_boot.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-# script to be run on first boot of instance
+# startup script for kaldi instance
 
-# pull repository
-cd /home
-git clone https://github.com/michaelcapizzi/kaldi.git
-wait
-cd kaldi
-git checkout kaldi_instructional
-wait
+# make sure codebase is up-to-date
+cd /scratch
+if [ ! -d "${KALDI_PATH}" ]; then
+  git clone https://github.com/michaelcapizzi/kaldi.git
+  wait
+  cd kaldi
+  git checkout kaldi_instructional
+else
+  git checkout kaldi_instructional
+  wait
+  git pull
+fi
 
-# back up /etc/group
-cp /etc/group /etc/group.original
-wait
-
-# add mcapizzi and bjoyce to to /etc/group
-sed -i.bak -E "s%(users:x:[0-9]+:[a-z,]+)%\1mcapizzi,bjoyce3,%" /etc/group
+# make sure bjoyce3 and mcapizzi are in users
+added_to_groups=$(cat /etc/group | grep bjoyce3)
+if [ -z ${added_to_groups} ]; then
+  cp /etc/group /etc/group.original
+  wait
+  sed -i.bak -E "s%(users:x:[0-9]+:[a-z,]+)%\1mcapizzi,bjoyce3,%" /etc/group
+fi

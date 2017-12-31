41e1aa337529a51c58d476edad25bb6182ac7cab
Michael Capizzi <mcapizzi@email.arizona.edu>
2017-12-31 07:03:33 -0700

    updating environ path

--- egs/INSTRUCTIONAL/utils/feature_viz/kaldi_io_for_python/kaldi_io.py
+++ egs/INSTRUCTIONAL/utils/feature_viz/kaldi_io_for_python/kaldi_io.py
@@ -11,7 +11,7 @@ import sys, os, re, gzip, struct
 # Select kaldi,
 if not 'KALDI_ROOT' in os.environ:
   # Default! To change run python with 'export KALDI_ROOT=/some_dir python'
-  os.environ['KALDI_ROOT']='/mnt/matylda5/iveselyk/Tools/kaldi-trunk'
+  os.environ['KALDI_ROOT']='/scratch/kaldi'
 
 # Add kaldi tools to path,
 os.environ['PATH'] = os.popen('echo $KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lm/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$KALDI_ROOT/src/nnetbin:$KALDI_ROOT/src/nnet2bin:$KALDI_ROOT/src/nnet3bin:$KALDI_ROOT/src/online2bin/:$KALDI_ROOT/src/ivectorbin/:$KALDI_ROOT/src/lmbin/').readline().strip() + ':' + os.environ['PATH']

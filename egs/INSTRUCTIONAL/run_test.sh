89dd80ff75a118ffb5b199e60b39f14ec69eb692
Michael Capizzi <mcapizzi@email.arizona.edu>
2018-03-05 10:28:32 -0700

    beforee pull

--- egs/INSTRUCTIONAL/run_test.sh
+++ egs/INSTRUCTIONAL/run_test.sh
@@ -88,7 +88,7 @@ seconds=$(expr ${elapsed} - ${minutes} \* 60)
 # analyze lattices
 for lmwt in $(seq ${weight_lower} ${weight_upper}); do
     # convert lmwt -> acwt
-    acwt=$(awk "BEGIN {printf \"%.1f\",1.0/${lmwt}}")
+    acwt=$(awk "BEGIN {printf \"%.2f\",1.0/${lmwt}}")
     ${KALDI_INSTRUCTIONAL_PATH}/steps/diagnostic/analyze_lats.sh \
         --acwt ${acwt} \
         ${graph_dir} \
